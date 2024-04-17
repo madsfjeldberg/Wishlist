b1f6d6ab7df279c9bb0faf7504d992d609d2016e
madsfjeldberg <mafj0001@stud.kea.dk>
2024-04-17 19:29:29 +0200

    updated docs and sql scripts

--- src/test/resources/h2init.sql
+++ src/test/resources/h2init.sql
@@ -1,7 +1,5 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS wishes;
-DROP TABLE IF EXISTS wish_tags;
-DROP TABLE IF EXISTS all_tags;
 
 CREATE TABLE users (
      user_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -15,19 +13,8 @@ CREATE TABLE wishes (
     name VARCHAR(255) NOT NULL,
     price FLOAT NOT NULL,
     link VARCHAR(255),
-    shop VARCHAR(255)
-);
-
-CREATE TABLE wish_tags (
-    tag_number INT AUTO_INCREMENT PRIMARY KEY,
-    wish_id INT NOT NULL,
-    tag_id INT NOT NULL
-);
-
-CREATE TABLE all_tags (
-    tag_id INT AUTO_INCREMENT PRIMARY KEY,
-    wish_id INT NOT NULL,
-    tag_value VARCHAR(255) NOT NULL
+    reserved_username VARCHAR(255),
+    is_reserved BOOLEAN DEFAULT FALSE
 );
 
 INSERT INTO users (username, password) VALUES
@@ -36,13 +23,13 @@ INSERT INTO users (username, password) VALUES
     ('user2', 'password2'),
     ('user3', 'password3');
 
-INSERT INTO wishes (user_id, name, price, link, shop) VALUES
-    (1, 'Macbook Pro', 2000, 'https://www.apple.com/dk/', 'Apple'),
-    (1, 'iPhone 12', 800, 'https://www.apple.com/dk/', 'Apple'),
-    (2, 'Airpods Pro', 200, 'https://www.apple.com/dk/', 'Apple'),
-    (2, 'Apple Watch', 400, 'https://www.apple.com/dk/', 'Apple'),
-    (3, 'Macbook Air', 1200, 'https://www.apple.com/dk/', 'Apple'),
-    (3, 'iPad Pro', 1000, 'https://www.apple.com/dk/', 'Apple');
+INSERT INTO wishes (user_id, name, price, link) VALUES
+    (1, 'Macbook Pro', 2000, 'https://www.apple.com/dk/'),
+    (1, 'iPhone 12', 800, 'https://www.apple.com/dk/'),
+    (2, 'Airpods Pro', 200, 'https://www.apple.com/dk/'),
+    (2, 'Apple Watch', 400, 'https://www.apple.com/dk/'),
+    (3, 'Macbook Air', 1200, 'https://www.apple.com/dk/'),
+    (3, 'iPad Pro', 1000, 'https://www.apple.com/dk/');
 
 
 
